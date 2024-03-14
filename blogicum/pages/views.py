55784ea4752c9087c87314567397318c2701fb24
DmitryShiyanov <dm1try.shiyanov@yandex.ru>
2024-03-14 18:56:27 +0300

    new_one

---
+++ blogicum/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, template_name = 'pages/about.html')
+
+
+def rules(request):
+    return render(request, template_name = 'pages/rules.html')
+
+# Create your views here.
